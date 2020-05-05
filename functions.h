cb10ad69e9ec1348578de5249e2b38b04c68c13a
mario9881 <mario_9881@abv.bg>
2020-05-05 15:07:28 +0300

    Add trim function

--- functions.h
+++ functions.h
@@ -25,4 +25,16 @@ bool isInteger(const string& s) {
     return true;
 }
 
+// Source:
+// https://stackoverflow.com/questions/25829143/trim-whitespace-from-a-string/25829178
+string trim(const string& str) {
+    int first = str.find_first_not_of(" \n\t");
+    if (first == string::npos) {
+        return str;
+    }
+    int last = str.find_last_not_of(" \n\t");
+    int substr_length = last - first + 1;
+    return str.substr(first, substr_length);
+}
+
 #endif
