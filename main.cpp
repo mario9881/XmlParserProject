fd591131c4769ffda3b7a2e97e536ee7da48e186
mario9881 <mario_9881@abv.bg>
2020-05-18 14:42:17 +0300

    Uncomment the commands

--- main.cpp
+++ main.cpp
@@ -6,6 +6,17 @@
 using namespace std;
 
 int main() {
+
+    string s;
+
+    while(true) {
+        cin >> s;
+    }
+
+    return 0;
+
+
+
     XMLFile file01;
     file01.openFile("example.xml");
     return 0;
@@ -25,7 +36,7 @@ int main() {
         else if (command == "close") {
             file.closeFile();
         }
-        /*else if (command == "save") {
+        else if (command == "save") {
             file.saveFile();
         }
         else if (command == "save_as") {
@@ -34,8 +45,7 @@ int main() {
         else if (command == "help") {
             file.help();
         }
-    
-    
+     
         else if (command == "print") {
             file.printXML();
         }
@@ -46,23 +56,23 @@ int main() {
             file.setAttribute();
         }
         else if (command == "children") {
-            file.childrenlist();
+            file.childrenList();
         }
         else if (command == "child") {
             file.printChild();
         }
         else if (command == "text") {
-            file.printElementtext();
+            file.printElementText();
         }
         else if (command == "delete") {
             file.deleteAttribute();
         }
         else if (command == "newchild") {
-            file.newchild();
+            file.newChild();
         }
         else if (command == "xpath") {
             file.xpath();
-        }*/
+        }
         else if (command == "exit") {
             weHaveEnteredExit = true;
         }
